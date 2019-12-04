8df1aca3aff26cfd71911aced11c58d4eab9f839
Joan Andrés <andres6936@live.com>
2019-12-04 14:12:47 -0500

    Added new sizerLayout to Interfaz

--- Source/Interfaz/InterfazElecciones.cpp
+++ Source/Interfaz/InterfazElecciones.cpp
@@ -8,17 +8,25 @@ InterfazElecciones::InterfazElecciones( const wxString &title ) : wxFrame( nullp
 
     wxBoxSizer *sizerLayoutRoot = new wxBoxSizer( wxVERTICAL );
     wxBoxSizer *sizerLayoutCandidatos = new wxBoxSizer( wxHORIZONTAL );
+    wxStaticBoxSizer *sizerLFrank = new wxStaticBoxSizer( wxVERTICAL, this, wxT( "Candidato Frank" ) );
+    wxStaticBoxSizer *sizerLClaire = new wxStaticBoxSizer( wxVERTICAL, this, wxT( "Candidato Claire" ) );
+    wxStaticBoxSizer *sizerLObama = new wxStaticBoxSizer( wxVERTICAL, this, wxT( "Candidato Obama" ) );
 
     panelImagen = new PanelImagen( this );
 
     panelFrank = new PanelCandidato( this, urna.getCandidato1( ));
+    sizerLFrank->Add( panelFrank, 1, wxALIGN_CENTER );
+
     panelClaire = new PanelCandidato( this, urna.getCandidato2( ));
+    sizerLClaire->Add( panelClaire, 1, wxALIGN_CENTER );
+
     panelObama = new PanelCandidato( this, urna.getCandidato3( ));
+    sizerLObama->Add( panelObama, 1, wxALIGN_CENTER );
 
     sizerLayoutRoot->Add( panelImagen, 1, wxEXPAND );
-    sizerLayoutCandidatos->Add( panelFrank, 1, wxEXPAND );
-    sizerLayoutCandidatos->Add( panelClaire, 1, wxEXPAND );
-    sizerLayoutCandidatos->Add( panelObama, 1, wxEXPAND );
+    sizerLayoutCandidatos->Add( sizerLFrank, 1, wxEXPAND );
+    sizerLayoutCandidatos->Add( sizerLClaire, 1, wxEXPAND );
+    sizerLayoutCandidatos->Add( sizerLObama, 1, wxEXPAND );
     sizerLayoutRoot->Add( sizerLayoutCandidatos, 1, wxEXPAND );
 
     this->SetSizerAndFit( sizerLayoutRoot );
